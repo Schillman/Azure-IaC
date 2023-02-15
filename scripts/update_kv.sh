ae2e9cd79b857fc885927a6c1d5605efc93fdb77
Sebastian Schillman <sebastian@schillman.se>
2023-02-16 00:19:29 +0100

    minor changes to script and added decoding step to workflow

--- scripts/update_kv.sh
+++ scripts/update_kv.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 function update_kv_secret {
-	base64 tf_secrets_mini.json >tf_secrets_base64.txt
+	base64 tf_secrets-min.json >tf_secrets_base64.txt
 	echo 'file converted to base64'
-	az keyvault secret set --name tf-secret-base64 --vault-name $1 --file tf_secrets_base64.txt --encoding base64
+	az keyvault secret set --name tf-secrets-base64 --vault-name $1 --file tf_secrets_base64.txt --encoding base64
 }
 
 update_kv_secret kv-schillman-management
