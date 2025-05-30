32d2c4a46f6a6a30d019d4c5a1abc224227c9851
Sebastian Schillman <sebastian.ext.schillman@swedbank.se>
2025-05-30 16:22:38 +0200

    fix: correct base64 command for macOS

--- scripts/update_kv.sh
+++ scripts/update_kv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 function update_kv_secret {
-	base64 tf_secrets-min.json >tf_secrets_base64.txt
+	base64 -i tf_secrets-min.json -o tf_secrets_base64.txt
 	echo 'file converted to base64'
 	az keyvault secret set --name tf-secrets-base64 --vault-name $1 --file tf_secrets_base64.txt --encoding base64
 }
